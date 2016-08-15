d2d47c616fd63f96ad2c8a288368777cd3dc55da
Jannis R <mail@jannisr.de>
2016-08-15 16:17:13 +0200

    former positons of expressions

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const vm = require('vm')
 
 const inspect = (code) => {
 	const data = []
-	const hook = (x) => {data.push(x); return x}
+	const hook = (x, i) => {
+		if (data[i]) data[i].push(x);
+		return x
+	}
 
+	let i = 0
 	const tapped = falafel(code, {ecmaVersion: 6, ranges: true}, (n) => {
 		if (/Expression$/.test(n.type)) {
-			if (n.type === 'SequenceExpression')
-				n.update('_((' + n.source() + '))')
-			else n.update('_(' + n.source() + ')')
+			data[i] = [n.range[0], n.range[1]]
+			n.update('_((' + n.source() + '),' + i + ')')
+			i++
 		}
 	})
 
