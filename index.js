6f6429a4c8f7a6aeff90b2150b259b7dd2753dce
Jannis R <mail@jannisr.de>
2016-08-15 16:02:14 +0200

    simplify

--- index.js
+++ index.js
@@ -1,49 +1,23 @@
 'use strict'
 
-const astw = require('astw')
 const falafel = require('falafel')
 const vm = require('vm')
 
 
 
-const letters = 'abcdefghijklmnopqrstuvwxyz'
-const identifier = (blacklist) => {
-	while (true) {
-		let name = '_', i = 2
-		while (i-- > 0) name += letters[Math.random() * 25 | 0]
-		if (blacklist.indexOf(name) === -1) return name
-	}
-}
-
-const identifiers = (code) => {
-	const ids = []
-	astw(code)((node) => {
-		if (node.type === 'Identifier' && ids.indexOf(node.name) === -1)
-			ids.push(node.name)
-	})
-	return ids
-}
-
-
-
 const inspect = (code) => {
 	const data = []
-	const hookFn = (x) => {data.push(x); return x}
-	const hookName = identifier(identifiers(code))
+	const hook = (x) => {data.push(x); return x}
 
-	const tapped = falafel(code, {ecmaVersion: 6}, (n) => {
-		if (n.type === 'VariableDeclarator') {
-			if (n.init.type === 'SequenceExpression')
-				n.init.update(hookName + '((' + n.init.source() + '))')
-			else n.init.update(hookName + '(' + n.init.source() + ')')
-		} else if (n.type === 'AssignmentExpression') {
-			n.update(hookName + '(' + n.source() + ')')
-		} else if (n.type === 'ExpressionStatement') {
-			n.update(hookName + '(' + n.source() + ')')
+	const tapped = falafel(code, {ecmaVersion: 6, ranges: true}, (n) => {
+		if (/Expression$/.test(n.type)) {
+			if (n.type === 'SequenceExpression')
+				n.update('_((' + n.source() + '))')
+			else n.update('_(' + n.source() + ')')
 		}
 	})
 
-	const ctx = new vm.createContext({[hookName]: hookFn})
+	const ctx = new vm.createContext({_: hook})
 	const script = new vm.Script(tapped)
 	try {
 		script.runInContext(ctx)
