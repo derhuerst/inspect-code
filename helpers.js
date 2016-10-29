9007018e3cce8855082efe4ac04bb88c6604b973
Jannis R <mail@jannisr.de>
2016-10-29 17:23:03 +0200

    findEnclosingBlock helper

--- helpers.js
+++ helpers.js
@@ -18,10 +18,32 @@ const isPrimitiveExpression = (node) =>
 	/Expression$/.test(node.type)
 	&& !(/FunctionExpression$/.test(node.type))
 
+const isBlockStatement = (node) =>
+	node.type === 'BlockStatement'
+
+const isFunction = (node) =>
+	/FunctionExpression$/.test(node.type)
+	|| node.type === 'FunctionDeclaration'
+
+const enclosingFunctionBlock = (n) => {
+	let result = null
+	while (n.parent) {
+		n = n.parent
+		if (isBlockStatement(n) && n.parent && isFunction(n.parent)) {
+			result = n
+			break
+		}
+	}
+	return result
+}
+
 
 
 module.exports = {
 	unusedIdentifier,
 	isNamedCallExpression,
-	isPrimitiveExpression
+	isPrimitiveExpression,
+	isBlockStatement,
+	isFunction,
+	enclosingFunctionBlock
 }
